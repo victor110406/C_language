440c5981fea7d5fe7a3b6fa303022e94f37ae199
victor110406 <77650138+victor110406@users.noreply.github.com>
2022-05-13 14:55:35 +0900

    Add files via upload

---
+++ Increasing_and_decreasing.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main() {
+
+	int x = 10, y = 10;
+
+	printf("x의 값= %d\n", x);
+	printf("++x의 값= %d\n", ++x);
+	printf("x의 값= %d\n", x);
+
+	printf("y의 값= %d\n", y);
+	printf("y++의 값= %d\n", y++);
+	printf("y의 값= %d\n", y);
+
+	return 0;
+}
